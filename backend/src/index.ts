15014890b435c0b8711337869318ca468f63c8d4
Jv1-Z1 <zaiajavier1@gmail.com>
2025-11-27 22:49:06 -0300

    commit index,ts 2
    const PORT = Number(process.env.PORT) || 3000 commited, changing a string for a number to recieve

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ import productRoutes from './routes/productRoutes'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = Number(process.env.PORT) || 3000
 
 // Middlewares
 app.use(cors())
